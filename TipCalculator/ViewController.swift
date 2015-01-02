2e956da052679177e0d59deb7d152902dd9e6d57
Dan Hipschman <dsh@vicarious.com>
2015-01-01 16:34:43 -0800

    tipsy working

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -8,23 +8,94 @@
 
 import UIKit
 
+func strip(var s: String, c: String) -> String {
+    // Horrible :(
+    while (countElements(s) > 0 && s[s.startIndex] == Character(c)) {
+        // Can't have an empty range
+        if countElements(s) == 1 {
+            return ""
+        }
+        let range = Range(start: s.startIndex.successor(), end: s.endIndex)
+        s = s.substringWithRange(range)
+    }
+    return s
+}
+
 class ViewController: UIViewController {
+    
+    enum RoundingType {
+        case None
+        case Up
+        case Down
+        
+        func cycle() -> RoundingType {
+            switch self {
+            case .None:
+                return RoundingType.Up
+            case .Up:
+                return RoundingType.Down
+            case .Down:
+                return RoundingType.None
+            }
+        }
+        
+        func round(value: Double) -> Double {
+            switch self {
+            case .None:
+                return value
+            case .Up:
+                return ceil(value)
+            case .Down:
+                return floor(value)
+            }
+        }
+    }
 
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var percentageControl: UISegmentedControl!
+    var tipRounding = RoundingType.None
+    var totalRounding = RoundingType.None
     
     @IBAction func onEditingChange(sender: AnyObject) {
-        let billAmount = (billField.text as NSString).doubleValue
+        var billString = strip(strip(billField.text, "$"), "0")
+        if countElements(billString) == 0 {
+            billString = "0"
+        }
+        let billAmount = (billString as NSString).doubleValue
         let percentageIndex = percentageControl.selectedSegmentIndex
         let percentage = [0.18, 0.20, 0.22][percentageIndex]
-        let tipAmount = billAmount * percentage
-        let totalAmount = billAmount + tipAmount
+        var tipAmount = tipRounding.round(billAmount * percentage)
+        let totalAmount = totalRounding.round(billAmount + tipAmount)
+        if (totalAmount != billAmount + tipAmount) {
+            tipAmount = totalAmount - billAmount
+        }
         tipLabel.text = String(format: "$%.2f", tipAmount)
         totalLabel.text = String(format: "$%.2f", totalAmount)
+        billField.text = "$" + billString
     }
 
+    @IBAction func onTipTap(sender: AnyObject) {
+        tipRounding = tipRounding.cycle()
+        if tipRounding != .None {
+            totalRounding = .None
+        }
+        onEditingChange(0)
+    }
+    
+    @IBAction func onTotalTap(sender: AnyObject) {
+        totalRounding = totalRounding.cycle()
+        if totalRounding != .None {
+            tipRounding = .None
+        }
+        onEditingChange(0)
+    }
+
+    @IBAction func onViewTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
